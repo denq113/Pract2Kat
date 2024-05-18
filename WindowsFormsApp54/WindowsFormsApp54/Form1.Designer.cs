e3e9d88aaf2b2e6c96b71ede8ae4347ad7fe0a80
denq113 <86840750+denq113@users.noreply.github.com>
2024-05-18 19:49:16 +0700

    helo

---
+++ WindowsFormsApp54/WindowsFormsApp54/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WindowsFormsApp54
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(19, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(605, 411);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(668, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(658, 115);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(79, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Гипербола";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(658, 160);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Парабола";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(658, 83);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ WindowsFormsApp54/WindowsFormsApp54/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp54
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                hiperbola();
+            }
+            if(radioButton2.Checked == true)
+            {
+               parabola();
+            }
+
+        }
+        
+        public void parabola()
+        {
+            int ky = 30;
+            int kx = 30;
+            int dy = pictureBox1.Height / 2;
+            int dx = pictureBox1.Width / 2;
+            Bitmap privet = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = privet;
+            Graphics cute = Graphics.FromImage(privet);
+            Pen obwodka = new Pen(Color.Pink);
+            obwodka.Width = 3;
+            SolidBrush solidBrush = new SolidBrush(Color.White);
+            cute.FillRectangle(solidBrush, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            Point[] mas = new Point[0];
+            int i = 0;
+            for (double x = -10; x <= 10; x = x + 0.1)
+            {
+                Array.Resize(ref mas, i + 1);
+                double y = -x*x;
+                mas[i] = new Point(Convert.ToInt32(x * kx) + dx, (Convert.ToInt32(y * ky) + dy));
+                i++;
+            }
+
+            Pen pl = new Pen(Color.Black);
+            pl.Width = 2;
+            cute.DrawLine(pl, 0, dy, pictureBox1.Width, dy);
+            cute.DrawLine(pl, dx, 0, dx, pictureBox1.Height);
+            cute.DrawLines(obwodka, mas);
+            pictureBox1.Refresh();
+        }
+
+        private void hiperbola()
+        {
+            int a = Convert.ToInt32(numericUpDown1.Value);
+            int ky = 30;
+            int kx = 30;
+            int dy = pictureBox1.Height / 2;
+            int dx = pictureBox1.Width / 2;
+            Bitmap privet = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = privet;
+            Graphics cute = Graphics.FromImage(privet);
+            Pen obwodka = new Pen(Color.Pink);
+            obwodka.Width = 3;
+            SolidBrush solidBrush = new SolidBrush(Color.White);
+            cute.FillRectangle(solidBrush, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            Point[] mas = new Point[0];
+            int i = 0;
+            for (double x = 0.1; x <= 10; x = x + 0.1)
+            {
+                Array.Resize(ref mas, i + 1);
+                double y = -a / x;
+                mas[i] = new Point(Convert.ToInt32(x * kx) + dx, (Convert.ToInt32(y * ky) + dy));
+                i++;
+            }
+
+            Pen pl = new Pen(Color.Black);
+            pl.Width = 2;
+            cute.DrawLine(pl, 0, dy, pictureBox1.Width, dy);
+            cute.DrawLine(pl, dx, 0, dx, pictureBox1.Height);
+            cute.DrawLines(obwodka, mas);
+            pictureBox1.Refresh();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked==true)
+            {
+                hiperbola();
+            }
+        }
+    }
+}
